3e9162521ceb3fb9f677fb9390663cf4968d4980
edapigo <edwardapg@gmail.com>
2021-03-23 03:22:51 -0500

    Images fix . Start Vallidation
    A minor issue in the url of each images of the project. Some validattions for add task window

--- Scheduler/src/scheduler/AnchorPaneNode.java
+++ Scheduler/src/scheduler/AnchorPaneNode.java
@@ -5,13 +5,16 @@ import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -35,13 +38,18 @@ public class AnchorPaneNode extends AnchorPane {
             @Override
             public void handle(MouseEvent e) {
                 System.out.println("This pane's date is: " + date);
+                
                 Stage stage = new Stage();
                 stage.initStyle(StageStyle.UNDECORATED);
                 Stage window = (Stage) ((Node) e.getSource()).getScene().getWindow();               
                 window.close();
 
                 try {
+                    
                     Parent root = FXMLLoader.load(AnchorPaneNode.this.getClass().getResource("AddingTask.fxml"));
+                    
+                    fillDatePicker((Pane) root);
+                    
                     stage.setScene(new Scene(root));
                     stage.show();
                 } catch (IOException ex) {
@@ -50,7 +58,19 @@ public class AnchorPaneNode extends AnchorPane {
             }
         });
     }
-
+    
+    /**
+     *  A methon to fill the DatePicker with the Date pressed
+     * @param container
+     */
+    public void fillDatePicker(Pane container) {
+        for (Node child : container.getChildren()) {
+            if (child instanceof DatePicker) {
+                ((DatePicker) child).setValue(LocalDate.of(date.getYear(), date.getMonth(), date.getDayOfMonth()));
+            } 
+        }
+    }
+    
     public LocalDate getDate() {
         return date;
     }

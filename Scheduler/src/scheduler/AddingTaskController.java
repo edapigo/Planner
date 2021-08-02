59fef2df2035598dd73e8d41ccfc51536596ed30
Juan Xavier Pita <jx_pita17@hotmail.com>
2021-08-01 20:58:25 -0500

    Update AddingTaskController.java

--- Scheduler/src/scheduler/AddingTaskController.java
+++ Scheduler/src/scheduler/AddingTaskController.java
@@ -29,19 +29,21 @@ import javafx.stage.Stage;
 public class AddingTaskController implements Initializable {
 
     @FXML
-    TextField title;
+    private TextField title;
     @FXML
-    ComboBox startTime;
+    private ComboBox startTime;
     @FXML
-    ComboBox endTime;
+    private ComboBox endTime;
     @FXML
-    DatePicker startDate;
+    private DatePicker startDate;
     @FXML
-    DatePicker endDate;
+    private DatePicker endDate;
     @FXML
-    TextArea details;
+    private TextArea details;
+    
+    private ArrayList<Account> participants;
 
-    String[] hours = new String[]{
+    private String[] hours = new String[]{
         "0:00", "0:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30",
         "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
         "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"
@@ -74,6 +76,10 @@ public class AddingTaskController implements Initializable {
     
     public void reminder(accounts ArrayList<Account>) {
     
+    }
+    
+    public void addParticipant(Account user) {
+        this.participants.add(user);
     }
 
 //    public boolean 
